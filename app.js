33533709e95883d5b807cd3683cf8ca2c191c27b
allenwalker1106 <vuthinh.UET@gmail.com>
2019-11-21 08:29:19 +0700

    https://github.com/allenwalker1106/OnlineExam.git

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const path=require('path');
@@ -6,21 +7,16 @@ const MongoConnector = require('./mongodbQuery.js').MongoConnector;
 const Schema = require('./mongodbQuery.js').Schema;
 const DBProcedure = require('./mongodbQuery.js').DBProcedure;
 
-MongoConnector.connect('mongodb://localhost:27017/media');
+var expressSession = require('express-session');
 
-
-const testdata={
-    user_info: {
-        name: { firstName: 'asssd', middleName: 'asdasd', lastName: 'asdasd' },
-        username: 'allen',
-        password: '123',
-        email: 'asddddddssasdddasdasd@gmail.com',
-        date_of_birth: '2019-11-10',
-        gender: { male: 'on' },
-        account_type: { taker: 'on' },
-        country: 'Armenia'
-    } 
-};
+//new commit 
+// MongoConnector.connect('mongodb://localhost:27017/media');
+MongoConnector.connect('mongodb+srv://coldblood101:Dragon1774@mastercluster-lhsxk.azure.mongodb.net/onlineexam?retryWrites=true&w=majority')
+app.use(expressSession({
+    secret: 'secretaakey',
+    resave: false,
+    saveUninitialized: true
+}));
 
 const PORT = 3000;
 app.use(express.static("public"));
@@ -29,74 +25,117 @@ app.set('views',__dirname+"/public/views");
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+var session = require('express-session');
+
+
 app.get('/',(req,res)=>{
     res.render('home');
 });
-
-app.get('/help',(req,res)=>{
-    res.render('help');
-})
-
-app.get('/howitwork',(req,res)=>{
-    res.render('howitwork');
-})
-
 app.get('/signin',(req,res)=>{
     res.render('signin',{user:{username:""}});
 })
-
-
-app.post('/signup',(req,res)=>{
-    DBProcedure.userSignup(req.body);
-    res.redirect('/signin')
-})
-
-
-
-app.post('/signinAccount',(req,res)=>{
-    console.log(req.body);
-    DBProcedure.getUser(req.body).countDocuments().exec((err,result)=>{
+app.post('/signin',(req,res)=>{
+    DBProcedure.getUser(req.body.user_info).countDocuments().exec((err,result)=>{
         if(err) throw err;
         if(result!=1){
-            console.log({username:req.body.user_info.username});
-            res.render('signin',{user:{username:req.body.user_info.username}});
+            res.render('signin',{user:{username:req.body.user_info.user_info.username}});
             
         }
         else{
-            DBProcedure.getUser(req.body).exec((err,result)=>{
+            DBProcedure.getUser(req.body.user_info).exec((err,result)=>{
                 if(err) throw err;
-                console.log(result);
-                res.render('front_page',{user:result});
+                var sessData = req.session;
+                sessData.user = result;
+                res.render('dashboard',{user:result});
             })
         }
     });
 })
-
-
-
-
-
-
-
-
-
-
-
+app.post('/signup',(req,res)=>{
+    DBProcedure.userSignup(req.body.user_info);
+    res.redirect('/signin')
+})
 
 app.get('/test',(req,res)=>{
-    DBProcedure.getUser(testdata).exec((err,result)=>{
+    if(req.session.user){
+        var p = getQuestionBank(req.session.user);
+        p.exec((err,rest)=>{
         if(err) throw err;
-        console.log(req.body);
-        res.render('home',{user:result});
-    
+        console.log(rest);
     })
+    }else
+        res.redirect('/signin');
+    
 })
 
-app.get('/show',(req,res)=>{
-    DBProcedure.getUser(testdata)
-    // res.redirect('/');
+
+app.get('/dashboard',(req,res)=>{
+    if(req.session.user){
+        res.render('dashboard',{user:req.session.user});
+    }else
+        res.render('signin',{user:{username:""}});
 })
 
+
+
+
+app.get('/qchoice',(req,res)=>{
+    if(req.session.user){
+        res.render('qchoice',{user:req.session.user})
+    }
+});
+
+app.post('/qchoice',(req,res)=>{
+    //create question
+    console.log(req.body.user_info);
+    DBProcedure.addQuestion(req.body.user_info);
+    res.render('qchoice',{user:req.session.user})
+})  
+
+
+app.get('/questions',(req,res)=>{
+    res.render('questions',{user:req.session.user,questions:[{topic:'topic',title:' title',type:'type',create_date:'create-date',last_modified:'modified',result:'result',import_in:[{name:'test1'},{name:'test2'}]},{topic:'topic',title:' title',type:'type',create_date:'create-date',last_modified:'modified',result:'result',import_in:[{name:'test1'},{name:'test2'}]}]});
+})
+
+app.get('/tests',(req,res)=>{
+    res.render('tests',{user:req.session.user,tests:[{topic:'topic',description:' title',type:'type',create_date:'create-date',last_modified:'modified',result:'result',import_in:[{name:'test1'},{name:'test2'}]},{topic:'topic',description:' title',type:'type',create_date:'create-date',last_modified:'modified',result:'result',import_in:[{name:'test1'},{name:'test2'}]}]});
+})
+
+
+app.get('/links',(req,res)=>{
+    res.render('links',{user:req.session.user,links:[{link_name:'link1',test_name:'test1'},{link_name:'link2',test_name:'test2'}]});
+})
+
+
+app.get('/dashboard',(req,res)=>{
+    res.render('dashboard',{user:req.session.user});
+})
+
+
+app.get('/groups',(req,res)=>{
+    res.render('groups',{user:req.session.user,groups:[{name:'group1',members:["1","2","3","4"]},{name:'group2',members:["1","2","3","4","5","6"]},{name:'group3',members:["1","2",]}]});
+})
+
+
+app.get('/results',(req,res)=>{
+    res.render('results',{user:req.session.user,results:[{test_name:'test1',members:["1","2","3","4","5","6"]},{test_name:'test2',members:["1","2",]}]});
+})
+
+
+app.get('/files',(req,res)=>{
+    res.render('upload_files',{user:req.session.user,files:[{question_id:'q1',data_link:'link1',type:'.jpeg'},{question_id:'q2',data_link:'link2',type:'.png'},{question_id:'q3',data_link:'link3',type:'.mp3'}]});
+})
+
+app.get('/user',(req,res)=>{
+    res.render('profile',{user:req.session.user,username: 'allen'})
+})
+
+
+app.post('/createQChoice',(req,res)=>{
+    res.redirect('create_question');
+})
+app.get('')
 
 app.listen(PORT,()=>{
     console.log('App running on port' + PORT.toString());
--- mongodbQuery.js
+++ mongodbQuery.js
@@ -3,17 +3,17 @@ const validator = require('validator')
 
 const userSchema = new Mongoose.Schema({
     name:{
-        firstName: {
+        firstname: {
             type: String,
             required: true,
             lowercase: false
         },
-        middleName:{
+        middlename:{
             type:String,
             default:"",
             required: false,
         },
-        lastName:{
+        lastname:{
             type: String,
             required: true,
             lowercase : false
@@ -50,10 +50,21 @@ const userSchema = new Mongoose.Schema({
         required: true,
         lowercase: true
     },
-    country:{
+    profile_image_link:{
         type: String,
-        required: true,
-        lowercase: true,
+        default: "/data/image/default_avata.png"
+    },
+    address:{
+        city:{
+            type: String,
+            lowercase: true,
+        },
+        country:{
+            type: String,
+            required: true,
+            lowercase: true,
+        }
+
     },
     courses:[
         {
@@ -75,27 +86,311 @@ const userSchema = new Mongoose.Schema({
     ],
     results:[
         {
-            test:{
-                type:Mongoose.Schema.ObjectId,
-                ref: 'Tests'
-            },
-            result:{
-                type:Mongoose.Schema.ObjectId,
-                ref:'Results'
-            },
+            type:Mongoose.Schema.ObjectId,
+            ref:'Results'
+        }
+    ],
+    links:[
+        {
+            type: Mongoose.Schema.ObjectId,
+            ref: 'Links',
+        }
+    ],
+    files:[
+        {
+            type: Mongoose.Schema.ObjectId,
+            ref: 'Files',
         }
     ]
-    
-
 })
+
 const UserModel = Mongoose.model('User',userSchema);
 
+const courseSchema = new Mongoose.Schema({
+    name:{
+        type: String,
+        min: 6,
+        max: 255,
+        required: true
+    },
+    administrator:{
+        type:Mongoose.Schema.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    members:[{
+        type:Mongoose.Schema.ObjectId,
+        ref:'User'
+    }],
+    create_date:{
+        type: Date,
+        default:Date.now
+    }
 
+})
+const CourseModel = Mongoose.model('Course', courseSchema, 'courses');
+
+const resultSchema= new Mongoose.Schema({
+	name:{
+		type:String,
+		required:true
+	},
+	owner:{
+		type: Mongoose.Schema.ObjectId,
+		ref: 'Users'
+	},
+	result:[{
+		question_id:{
+			type:Mongoose.Schema.ObjectId,
+			required: true
+		},
+		answear:{
+			type:Object,
+			required:true
+		}
+	}]
+})
+
+const ResultModel=('Result',resultSchema,"results");
+const testSchema =  new Mongoose.Schema({
+	name: {
+		type: String,
+		required:true,
+		min: 6,
+		max: 255
+	},
+	desciption: {
+		type:String,
+		min: 6,
+		max : 1024
+	},
+	questions:[{
+		type: Mongoose.Schema.ObjectId,
+		ref:'Questions'
+	}],
+	create_date:{
+		type:Date,
+		default: Date.now
+	},
+	owner:{
+		type: Mongoose.Schema.ObjectId,
+		ref: 'Users'
+	}
+})
+
+const TestModel =Mongoose.model('Test',testSchema,'tests');
+
+
+const qchoice_schema = new Mongoose.Schema({
+	type:{
+		type:String,
+		required: true,
+		default:"choices"
+	},
+	topic:{
+		type:String,
+		required:true,
+		default:'general'
+	},
+	owner:{
+		type:Mongoose.Schema.ObjectId,
+		ref:'Users'
+	},
+	max_choices:{
+		type:Number, 
+		min:1,
+		default:1,
+		required: true
+	},
+	body:{
+		type:String,
+		required: true,
+	},
+	imported:[{
+		type:Mongoose.Schema.ObjectId,
+	}],
+	options:{
+		type:Array,
+		default:[],
+		required: true
+	},
+	result:{
+		type:Array,
+		default:[],
+		required: true
+	}
+})
+
+
+// const QChoices = Mongoose.model('QChoices',qchoice_schema,'questions');
+
+const Question = Mongoose.model('QChoices',qchoice_schema,'questions');
+
+const qcode_schema = new Mongoose.Schema({
+	type:{
+		type:String,
+		required: true,
+		default:"code"
+	},
+	topic:{
+		type:String,
+		required:true,
+		default:'general'
+	},
+	owner:{
+		type:Mongoose.Schema.ObjectId,
+		ref:'Users'
+	},
+	body:{
+		type:String,
+		required: true,
+	},
+	imported:[{
+		type:Mongoose.Schema.ObjectId,
+	}],
+	options:{
+	},
+	result:[
+		{
+			input:{
+				type:String,
+				required:true,
+				lowercase: true
+			},
+			out:{
+				type:String,
+				required:true,
+				lowercase: true
+			}
+		}
+	]
+})
+
+
+const QCOde = Mongoose.model('QCode',qcode_schema,'questions');
+
+
+
+const qmatching_schema = new Mongoose.Schema({
+	type:{
+		type:String,
+		required: true,
+		default:"matching"
+	},
+	topic:{
+		type:String,
+		required:true,
+		default:'general'
+	},
+	owner:{
+		type:Mongoose.Schema.ObjectId,
+		ref:'Users'
+	},
+	body:{
+		type:String,
+		required: true,
+	},
+	imported:[{
+		type:Mongoose.Schema.ObjectId,
+	}],
+	options:{
+		head:{
+			type:Array,
+			default:[],
+			required: true
+		},
+		tail:{
+			type:Array,
+			default:[],
+			required: true
+		}
+	},
+	result:[
+		{
+			head:{
+				type:String,
+				required:true,
+				lowercase: true
+			},
+			tail:{
+				type:String,
+				required:true,
+				lowercase: true
+			}
+		}
+	]
+})
+
+
+const QMatching = Mongoose.model('QMatching',qmatching_schema,'questions');
+
+
+
+const qvalidate_schema = new Mongoose.Schema({
+	type:{
+		type:String,
+		required: true,
+		default:"true_false"
+	},
+	topic:{
+		type:String,
+		required:true,
+		default:'general'
+	},
+	owner:{
+		type:Mongoose.Schema.ObjectId,
+		ref:'Users'
+	},
+	body:{
+		type:String,
+		required: true,
+	},
+	imported:[{
+		type:Mongoose.Schema.ObjectId,
+	}],
+	result:{
+		type: Boolean,
+		required: true,
+
+	}
+})
+
+
+const QValidate = Mongoose.model('QValidate',qvalidate_schema,'questions');
+
+createQuestion = function(data,user,type){
+    if(type==='choices'){
+        var options = []
+        for(var i =0 ;i<data.question.options.length ; i++)
+            if(data.question.options[i]!='')
+                options.push(data.question.options[i])
+            var question = new QChoices({
+            type:type,
+            topic:data.question.topic,
+            owner:user._id,
+            max_choices:1,
+            body:data.question.body,
+            options:options,
+            result:[options[parseInt(data.question.result)]]
+        })
+        question.save((err,res)=>{
+            if(err) throw errr;
+            var questions_list = user.questions;
+            questions_list.push(res._id);
+            user.questions.push(res._id);
+            console.log(questions_list);
+            UserModel.updateOne({ _id:user._id}, { $set: { questions: questions_list } },(err,res)=>{
+                if (err) throw err;
+            });
+            console.log('Susscess create question')
+        })
+    }
+}
 
 validateSignup= function(data){
     // overlap username
     // overlap email
-    return UserModel.find({$or:[{'username':data.user_info.username},{'email':data.user_info.email}]}).countDocuments();
+    return UserModel.find({$or:[{'username':data.username},{'email':data.email}]}).countDocuments();
 }
 
 userSignup = function(data){
@@ -103,26 +398,28 @@ userSignup = function(data){
         if(res==0)
         {
             let gender='other';
-            let account_type = data.user_info.account_type.taker ==='on' ? 'taker' : 'administrator';
-            if(data.user_info.gender.male==='on')
+            let account_type = data.account_type.taker ==='on' ? 'taker' : 'administrator';
+            if(data.gender.male==='on')
                 gender = 'male';
-            else if(data.user_info.gender.femmale==='on')
+            else if(data.gender.femmale==='on')
                 gender = 'female';
             
                 
             let user =new UserModel({
                 name:{
-                    firstName: data.user_info.name.firstName,
-                    middleName: data.user_info.name.middleName,
-                    lastName: data.user_info.name.lastName
+                    firstname: data.name.firstname,
+                    middlename: data.name.middlename,
+                    lastname: data.name.lastname
                 },
-                username: data.user_info.username,
-                password: data.user_info.password,
-                email   : data.user_info.email,
-                date_of_birth: data.user_info.date_of_birth,
+                username: data.username,
+                password: data.password,
+                email   : data.email,
+                date_of_birth: data.date_of_birth,
                 gender: gender,
                 account_type : account_type,
-                country: data.user_info.country
+                address:{
+                    country: data.address.country
+                }
             })
         
             user.save((err,res)=>{
@@ -135,15 +432,14 @@ userSignup = function(data){
     }); 
 }
 
-showAllUser= function(){
-    UserModel.find({}).exec((err,res)=>{
-        if(err) throw err;
-        console.log(res);
-    });
-}
 
 getUser=function(data){
-    return UserModel.findOne({'username':data.user_info.username,'password':data.user_info.password});
+    return UserModel.findOne({'username':data.username,'password':data.password});
+}
+
+getQuestionBank = function(data){
+    console.log(data.user_info);
+    return Question.find({owner:data._id});
 }
 
 exports.MongoConnector ={
@@ -164,9 +460,10 @@ exports.Schema={
 
 exports.DBProcedure={
     userSignup: userSignup,
-    showAllUser:showAllUser,
     validateSignup:validateSignup,
-    getUser:getUser
+    getUser:getUser,
+    getQuestionBank:getQuestionBank,
+    createQuestion:createQuestion
 }
 
 
